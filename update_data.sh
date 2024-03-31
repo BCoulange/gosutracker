d967e04edf1d3b64a53ad0f9a8b14afd55dca50f
Baptiste Coulange <baptiste.coulange@hey.com>
2024-03-31 09:37:48 +0200

    scripting

---
+++ update_data.sh
@@ -0,0 +1,6 @@
+python3 scrapping/1_main.py
+python3 scrapping/2_parse_gamelist.py
+python3 scrapping/3_get_games_infos.py
+python3 scrapping/4_scrap_these_pages.py
+python3 scrapping/5_scrap_reviews.py
+ruby scrapping/6_make_csv.rb
