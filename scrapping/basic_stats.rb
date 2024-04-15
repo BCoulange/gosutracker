6c0da0aa306400e082673f4bf15c6962dfe4acec
Baptiste Coulange <baptiste.coulange@hey.com>
2024-04-15 17:31:40 +0200

    update db

--- scrapping/basic_stats.rb
+++ scrapping/basic_stats.rb
@@ -220,6 +220,25 @@ base_game_configs = 8*7*6*5*4*3/6/6
 max_number_of_configs = to_ban*base_game_configs
 puts "max number of configs : #{max_number_of_configs}"
 puts "base game configs : #{base_game_configs}"
-nb_of_configs_seen = data.map{|d| [d["clans_banned"]] + [d["clans_selected"][0],d["clans_selected"][3],d["clans_selected"][4]].sort + [d["clans_selected"][1],d["clans_selected"][2],d["clans_selected"][5]].sort}.uniq.size
+nb_of_configs_seen = data.map{|d| d["clans_banned"] + [d["clans_selected"][0],d["clans_selected"][3],d["clans_selected"][4]].sort + [d["clans_selected"][1],d["clans_selected"][2],d["clans_selected"][5]].sort}.uniq.size
 
+
+groups = data.map{|d| d["clans_banned"] + [d["clans_selected"][0],d["clans_selected"][3],d["clans_selected"][4]].sort + [d["clans_selected"][1],d["clans_selected"][2],d["clans_selected"][5]].sort}.group_by{|el| el}
+# groups.sort_by{|k,v| -v.size}.each do |k,v|
+#   if v.size > 5
+#     puts "#{k} : #{v.size}"
+#   end
+# end 
+
+threshold = 10
+groups2 = groups.group_by{|k,v| v.size > threshold ? threshold : v.size}.sort_by{|k,v| -k}
+groups2.each do |k,v|
+  if k == threshold
+    puts "seen more than #{k} times : #{v.map{|el| el.size}.inject(:+)}"
+  else 
+    puts "seen #{k} times : #{v.map{|el| el.size}.inject(:+)}"
+  end
+end
 puts "#{(nb_of_configs_seen.to_f/max_number_of_configs*100).to_i}% configs seen (#{nb_of_configs_seen}/#{max_number_of_configs})"
+
+
