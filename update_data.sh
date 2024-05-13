094203c798f63447cd372103940afe4c60baef6c
Baptiste Coulange <baptiste.coulange@hey.com>
2024-05-13 17:58:02 +0200

    player stat victories

--- update_data.sh
+++ update_data.sh
@@ -4,4 +4,4 @@ python3 scrapping/3_get_games_infos.py
 python3 scrapping/4_scrap_these_pages.py
 python3 scrapping/5_scrap_reviews.py
 ruby scrapping/6_make_csv.rb
-ruby scrapping/7_anonymize.rb
+# ruby scrapping/7_anonymize.rb
